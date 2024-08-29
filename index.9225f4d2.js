d397a68d2b40eff2ae8d5b385e22fcc5123c07ba
sinner1993 <sergio1993goga@gmail.com>
2024-08-29 22:40:32 +0200

    make build

---
+++ index.9225f4d2.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");e.style.display="flex",e.style.justifyContent="center",e.style.alignItems="center",t.style.position="unset";
+//# sourceMappingURL=index.9225f4d2.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+wall.style.display = "flex";
+wall.style.justifyContent = "center";
+wall.style.alignItems = "center";
+spider.style.position = "unset";
+
+//# sourceMappingURL=index.f75de5e1.js.map
